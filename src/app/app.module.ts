418c4c291e3747dcde1b069488da0c615c19c7aa
unknown <tiagocalutis@hotmail.com.br>
2018-08-13 17:36:07 -0300

    Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
@@ -10,6 +13,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ContactProvider } from '../providers/contact/contact';
 
 @NgModule({
   declarations: [
@@ -21,7 +25,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyD7nLkAcgq_uKHHsdgoFAMUScufk74PSw0",
+      authDomain: "ionic-firebase-f38dc.firebaseapp.com",
+      databaseURL: "https://ionic-firebase-f38dc.firebaseio.com",
+      projectId: "ionic-firebase-f38dc",
+      storageBucket: "ionic-firebase-f38dc.appspot.com",
+      messagingSenderId: "1007897699776"})
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -34,7 +45,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ContactProvider
   ]
 })
 export class AppModule {}
